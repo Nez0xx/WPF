ff5d0bc4253f58c869e3098bc4af832103afe274
Max <retakt123@gmail.com>
2022-04-05 03:06:20 +0300

    lr 16 (save)- WIP

---
+++ lr16/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace lr16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvRaw = new System.Windows.Forms.DataGridView();
+            this.dgvSummary = new System.Windows.Forms.DataGridView();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.openFileDlg = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRaw)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSummary)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvRaw
+            // 
+            this.dgvRaw.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRaw.Location = new System.Drawing.Point(12, 12);
+            this.dgvRaw.Name = "dgvRaw";
+            this.dgvRaw.Size = new System.Drawing.Size(505, 317);
+            this.dgvRaw.TabIndex = 0;
+            // 
+            // dgvSummary
+            // 
+            this.dgvSummary.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSummary.Location = new System.Drawing.Point(523, 12);
+            this.dgvSummary.Name = "dgvSummary";
+            this.dgvSummary.Size = new System.Drawing.Size(409, 317);
+            this.dgvSummary.TabIndex = 1;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.AutoSize = true;
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnLoad.Location = new System.Drawing.Point(12, 335);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(187, 28);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Загрузить данные";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // openFileDlg
+            // 
+            this.openFileDlg.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 380);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dgvSummary);
+            this.Controls.Add(this.dgvRaw);
+            this.Name = "Form1";
+            this.Text = "Загрузка данных";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRaw)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSummary)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvRaw;
+        private System.Windows.Forms.DataGridView dgvSummary;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.OpenFileDialog openFileDlg;
+    }
+}
+
---
+++ lr16/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lr16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MaximizeBox = false;
+        }
+
+        public enum Building_Type
+        {
+            House,
+            Flat,
+            Room,
+            Undefined
+        }
+
+        public static Building_Type ToBuildingType(String type)
+        {
+            switch (type)
+            {
+                case "Дом": return Building_Type.House;
+                case "Квартира": return Building_Type.Flat;
+                case "Комната": return Building_Type.Room;
+                default: return Building_Type.Undefined;
+            }
+        }
+        public static String ToString(Building_Type type)
+        {
+            switch (type)
+            {
+                case Building_Type.House: return "Дом";
+                case Building_Type.Flat: return "Квартира";
+                case Building_Type.Room: return "Комната";
+                default: return "Не определено";
+            }
+        }
+
+        class RawDataItem  
+        {
+            public Building_Type type { get; set; }
+            public int rooms { get; set; }
+            public int size { get; set; }
+            public long price { get; set; }
+            public double mSqr_price
+            {
+                get { return (double)(price / size); }
+            }
+        }
+        class SummaryDataItem  
+        {
+            public int typeSum { get; set; }
+            public Building_Type type { get; set; }
+        }
+
+        interface DataInterface
+        {
+            List<RawDataItem> GetRawData();
+            List<SummaryDataItem> GetSummaryData();
+        }
+
+        class DataStorage : DataInterface
+        {
+            private List<RawDataItem> rawdata;
+            private List<SummaryDataItem> sumdata;
+            private char divider = '*';
+            public bool isRdy
+            {
+                get
+                {
+                    if (rawdata == null) return false;
+                    else return true;
+                }
+            }
+
+            private DataStorage() { }
+            private bool InitData(String datapath)
+            {
+                rawdata = new List<RawDataItem>();
+
+                try
+                {
+                    StreamReader sr = new StreamReader(datapath, Encoding.UTF8);
+                    String line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] items = line.Split(divider);
+                        String typeStr = items[0].Trim();
+                        var item = new RawDataItem()
+                        {
+                            type = ToBuildingType(items[0].Trim()),
+                            rooms = Convert.ToInt32(items[1].Trim()),
+                            size = Convert.ToInt32(items[2].Trim()),
+                            price = Convert.ToInt64(items[3].Trim())
+                        };
+                        rawdata.Add(item);
+                    }
+
+                    sr.Close();
+                    BuildSummary();
+                }
+
+                catch(IOException ex)
+                {
+                    MessageBox.Show(ex.Message, "Error");
+                    return false;
+                }
+
+                return true;
+            }
+            private void BuildSummary()
+            {
+                sumdata = new List<SummaryDataItem>();
+
+            }
+            public static DataStorage DataCreator(String path)
+            {
+                DataStorage d = new DataStorage();
+                if (d.InitData(path))
+                    return d;
+                else
+                    return null;
+            }
+            public List<RawDataItem> GetRawData()
+            {
+                if (this.isRdy)
+                    return rawdata;
+                else
+                    return null;
+            }
+            public List<SummaryDataItem> GetSummaryData()
+            {
+                if (this.isRdy)
+                    return sumdata;
+                else
+                    return null;
+            }
+        }
+    }
+}
